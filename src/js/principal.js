ce6d03c83ff19e5bc8cdad97996e4e11d348081d
Isaiah Quintana <isaiah.quintanase@elorrieta-errekamari.com>
2021-03-12 16:03:31 +0100

    Actualizar estructura

---
+++ src/js/principal.js
@@ -0,0 +1,12 @@
+function registro() {
+	window.open("registro.html");
+}
+
+function login() {
+	window.open("login.html");
+}
+
+function registrarUsuario() {
+	const nuevoUsuario = new usuario();
+	const miUsuario = document.getElementById("usuario").valueOf;
+}
